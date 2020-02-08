2057a023f615cd5aff57e0c3d157508d6d2694ac
Dmitry Harnitski <dharnitski@gmail.com>
2020-02-08 12:34:34 -0500

    validate url in post
    Signed-off-by: Dmitry Harnitski <dharnitski@gmail.com>

--- server/post/handler.go
+++ server/post/handler.go
@@ -3,6 +3,7 @@ package post
 import (
 	"encoding/json"
 	"net/http"
+	"net/url"
 )
 
 // Handler proceses
@@ -23,14 +24,29 @@ type Response struct {
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var req Request
 
+	// encode JSON
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// validate that URL is not empty
 	if req.URL == "" {
 		http.Error(w, "empty URL", http.StatusBadRequest)
+		return
+	}
+
+	// validate if URL is valid
+	parsed, err := url.Parse(req.URL)
+	if err != nil || parsed.Host == "" {
+		http.Error(w, "not valid URL", http.StatusBadRequest)
+		return
+	}
+
+	// validate URL scheme
+	if parsed.Scheme != "http" && parsed.Scheme != "https" {
+		http.Error(w, "only web links supported", http.StatusBadRequest)
 		return
 	}
 
--- server/post/handler_test.go
+++ server/post/handler_test.go
@@ -7,12 +7,14 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/dharnitski/url-shortener/post"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/dharnitski/url-shortener/post"
 )
 
 func TestPostHandler(t *testing.T) {
+	t.Parallel()
 	sut := post.Handler{}
 
 	r, err := http.NewRequest(http.MethodPost, "https://example.com/", bytes.NewBufferString(`{"url": "https://github.com/"}`))
@@ -26,30 +28,46 @@ func TestPostHandler(t *testing.T) {
 	assert.Equal(t, `{"shortenUrl":"qwe"}`, string(body))
 }
 
-func TestPostHandlerInvalidJson(t *testing.T) {
-	sut := post.Handler{}
+func TestPostHandlerInvalidInput(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"invalid",
+			"invalid character 'i' looking for beginning of value\n",
+		},
+		{
+			"{}",
+			"empty URL\n",
+		},
+		{
+			`{"url": "not url"}`,
+			"not valid URL\n",
+		},
+		{
+			`{"url": "😊"}`,
+			"not valid URL\n",
+		},
+		{
+			`{"url": "ftp://github.com/"}`,
+			"only web links supported\n",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			sut := post.Handler{}
 
-	r, err := http.NewRequest(http.MethodPost, "https://example.com/", bytes.NewBufferString(`invalid`))
-	require.NoError(t, err)
-	w := httptest.NewRecorder()
+			r, err := http.NewRequest(http.MethodPost, "https://example.com/", bytes.NewBufferString(test.input))
+			require.NoError(t, err)
+			w := httptest.NewRecorder()
 
-	sut.ServeHTTP(w, r)
-	assert.Equal(t, 400, w.Code)
-	body, err := ioutil.ReadAll(w.Body)
-	require.NoError(t, err)
-	assert.Equal(t, "invalid character 'i' looking for beginning of value\n", string(body))
-}
-
-func TestPostHandlerEmptyURL(t *testing.T) {
-	sut := post.Handler{}
-
-	r, err := http.NewRequest(http.MethodPost, "https://example.com/", bytes.NewBufferString(`{}`))
-	require.NoError(t, err)
-	w := httptest.NewRecorder()
-
-	sut.ServeHTTP(w, r)
-	assert.Equal(t, 400, w.Code)
-	body, err := ioutil.ReadAll(w.Body)
-	require.NoError(t, err)
-	assert.Equal(t, "empty URL\n", string(body))
+			sut.ServeHTTP(w, r)
+			assert.Equal(t, 400, w.Code)
+			body, err := ioutil.ReadAll(w.Body)
+			require.NoError(t, err)
+			assert.Equal(t, test.expected, string(body))
+		})
+	}
 }
