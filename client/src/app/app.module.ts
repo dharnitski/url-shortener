cd8af7ad901d343e9a1926622c6309f474fd92fb
Dmitry Harnitski <dharnitski@gmail.com>
2020-02-08 23:17:55 -0500

    add simple client UI
    Signed-off-by: Dmitry Harnitski <dharnitski@gmail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule, MatTooltipModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
